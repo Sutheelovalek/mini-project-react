f0f07041d2e744e8e30b1fe59432295e2a550235
suthee <lovalek@gmail.com>
2023-05-28 16:36:13 +0700

    feat: add function get data from DB

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,28 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [topic, setTopic] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      try {
+        const response = await axios.get(
+          `http://localhost:4002/trips?keywords=`
+        );
+        console.log(response.data.data);
+        setTopic(response.data.data);
+        
+      } catch (error) {
+        alert(error);
+        
+      }
+    };
+    getData();
+  }, [])
+
+
   return (
     <div className="App">
       <header className="travel-header">
@@ -21,26 +43,33 @@ function App() {
         </form>
       </header>
       <main className="travel-container">
-        <section className="travel-highlight">
-          <img className="travel-picture" alt="stunning view" />
+        {topic.map((place, index) => {
+          return (
+        <section 
+        key={index.eid}
+        className="travel-highlight">
+          <img src={place.photos[0]} className="travel-picture" alt="stunning view" />
           <section className="travel-list">
-            <h1 className="main-topics"></h1>
+            <h1 className="main-topics">{place.title}</h1>
             <div className="travel-description">
-              <div className="sub-topics"></div>
+              <h2 className="sub-topics">{place.description}</h2>
             </div>
             <div className="category-topics">
-              <span className="category-topic"></span>
-              <span className="category-add-and"></span>
-              <span className="category-before-and"></span>
+              <span className="category-topic">{place.tags.slice(0,-1).join(',')}</span>
+              <span className="category-add-and">และ</span>
+              <span className="category-before-and">{place.tags.slice(-1)}</span>
             </div>
             <div className="picture-groups">
-              <img className="picture-topics" alt="travel images" />
-              <img className="picture-topics" alt="travel images" />
-              <img className="picture-topics" alt="travel images" />
+              <img src={place.photos[1]} className="picture-topics" alt="travel images" />
+              <img  src={place.photos[2]} className="picture-topics" alt="travel images" />
+              <img src={place.photos[3]} className="picture-topics" alt="travel images" />
               <a className="link-highlight"></a>
             </div>
           </section>
         </section>
+            
+          );
+        })}
       </main>
     </div>
   );
