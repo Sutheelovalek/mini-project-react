882655a536c303d99b852dbf612860ff840a80fd
suthee <lovalek@gmail.com>
2023-06-09 09:18:14 +0700

    feat(homepage): add toggle and style css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,50 @@
 import { useEffect, useState } from "react";
 import "./App.css";
 import axios from "axios";
+import { FcLink } from 'react-icons/Fc'
 
 function App() {
   const [topic, setTopic] = useState([]);
+  const [searchTopic, setSearchTopic] = useState([]);
+  const [toggle, setToggle] = useState([]);
+
+
+ const handleInputChange = (event) => {
+  setSearchTopic(event.target.value);
+  handleSearch(event.target.value);
+ };
+
+ const handleSearch = async (searchId) => {
+  try {
+    const response = await axios.get(`http://localhost:4002/trips?keywords=${searchId}`
+    );
+    setTopic(response.data.data)
+    setToggle(Array(response.data.data).fill(false));
+  } catch (error) {
+    alert(error.message);
+  }
+ }
 
   useEffect(() => {
     const getData = async () => {
       try {
-        const response = await axios.get(
-          `http://localhost:4002/trips?keywords=`
-        );
+        const response = await axios.get("http://localhost:4002/trips?keywords=");
         console.log(response.data.data);
         setTopic(response.data.data);
-        
       } catch (error) {
         alert(error);
-        
       }
     };
     getData();
-  }, [])
-
-
+  }, []);
+  const handleToggle = (index, event) => {
+    event.preventDefault();
+    setToggle((prevToggles) => {
+      const newToggles = [...prevToggles];
+      newToggles[index] = !newToggles[index];
+      return newToggles;
+    });
+  };
   return (
     <div className="App">
       <header className="travel-header">
@@ -37,42 +59,58 @@ function App() {
               type="text"
               id="search"
               placeholder="หาที่เที่ยวแล้วไปกัน..."
+              value={searchTopic}
+              onChange={handleInputChange}
             />
             <hr className="travel-searching-line"></hr>
           </div>
         </form>
       </header>
       <main className="travel-container">
-        {topic.map((place, index) => {
+      {topic.map((place, index) => {
+        const shortDescription = place.description.slice(0, 100) + '...';
+        const toggleValue = toggle[index];
           return (
-        <section 
-        key={index.eid}
-        className="travel-highlight">
-          <img src={place.photos[0]} className="travel-picture" alt="stunning view" />
-          <section className="travel-list">
-            <h1 className="main-topics">{place.title}</h1>
-            <div className="travel-description">
-              <h2 className="sub-topics">{place.description}</h2>
-            </div>
-            <div className="category-topics">
-              <span className="category-topic">{place.tags.slice(0,-1).join(',')}</span>
-              <span className="category-add-and">และ</span>
-              <span className="category-before-and">{place.tags.slice(-1)}</span>
-            </div>
-            <div className="picture-groups">
-              <img src={place.photos[1]} className="picture-topics" alt="travel images" />
-              <img  src={place.photos[2]} className="picture-topics" alt="travel images" />
-              <img src={place.photos[3]} className="picture-topics" alt="travel images" />
-              <a className="link-highlight"></a>
-            </div>
-          </section>
+            <section key={place.eid} className="travel-highlight">
+              <img
+                className="travel-picture"
+                src={place.photos[0]}
+                alt="stunning view"
+              />
+              <section className="travel-list">
+                <h1 className="main-topics">{place.title}</h1>
+                <div className="travel-description">
+                  <div className="sub-topics">
+                     <div>{toggleValue ? place.description : shortDescription}</div>
+                  </div>
+                  <a
+                    href="#"
+                    onClick={(event) => handleToggle(index, event)}
+                    className="link-topics"
+                  >
+                    {toggleValue ? "น้อยลง" : "อ่านต่อ"}
+                  </a>
+                </div>
+        <div className="category-topics">
+          หมวด
+          <span className="category-topic">{place.tags.slice(0, -1).join(", ")}</span>
+          <span className="category-add-and"> และ </span> 
+          <span className="category-before-and">{place.tags.slice(-1)}</span>
+        </div>
+          <div className="picture-groups">
+            <img className="picture-topics" src={place.photos[1]} alt="travel images" />
+            <img className="picture-topics" src={place.photos[2]} alt="travel images" />
+            <img className="picture-topics" src={place.photos[3]} alt="travel images" />
+            <a href={place.url} className="link-highlight">
+            <FcLink />
+            </a>
+          </div>
         </section>
-            
-          );
-        })}
+        </section>
+      )})}     
       </main>
     </div>
-  );
-}
-
-export default App;
+    );
+  }
+  
+  export default App;
